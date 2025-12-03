67a46d915b3a07540c7f85f4c00564acd501eef4
Joe Dracup <joe@dracup.me.uk>
2025-12-03 12:36:59 +0000

    day 2 solution 2

--- solutions/Day02.cs
+++ solutions/Day02.cs
@@ -1,14 +1,51 @@
+
 public static class Day02
 {
     public static string Solve()
     {
         var solution1 = GetIdRanges("Day02.txt").Sum(r => r.Sum(GetInvalidIds));
-
+        var solution2 = GetIdRanges("Day02.txt").Sum(r => r.Sum(GetInvalidIds2));
         return @$"Day 2 
         Solution 1: {solution1}
-        Solution 2:  
+        Solution 2: {solution2}
         ";
     }
+
+    private static long GetInvalidIds2(this long id)
+    {
+        var s = id.ToString();
+
+        for (var i = 1; i < s.Length + 1; i++)
+        {
+            var sub = s.Substring(0, i);
+            if (sub == s)
+                return 0;
+
+            var count = s.AllIndexesOf(sub).Count();
+
+            if (count * sub.Length == s.Length)
+            {
+                return id;
+            }
+        }
+
+        return 0;
+    }
+
+    private static List<int> AllIndexesOf(this string str, string value)
+    {
+        if (String.IsNullOrEmpty(value))
+            throw new ArgumentException("the string to find may not be empty", "value");
+        List<int> indexes = new List<int>();
+        for (int index = 0; ; index += value.Length)
+        {
+            index = str.IndexOf(value, index);
+            if (index == -1)
+                return indexes;
+            indexes.Add(index);
+        }
+    }
+
 
     private static IEnumerable<IEnumerable<long>> GetIdRanges(this string fileName)
     {
